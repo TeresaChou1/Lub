8dede97f56bb4d77bad394549e590d3200b04cd9
Archer_小A <qddegtya@gmail.com>
2014-07-20 10:05:09 +0800

    Update terrain.py

--- lubdemo/webtc/support/terrain.py
+++ lubdemo/webtc/support/terrain.py
@@ -1,9 +1,9 @@
-#!usr/bin/env python
 # -*- coding:utf-8 -*-
+# controls
 
 from lettuce import *
 from selenium import webdriver
 
 @before.each_scenario
 def startupdriver(scenario):
-        world.browser=webdriver.Firefox()
+    world.browser=webdriver.Firefox()
